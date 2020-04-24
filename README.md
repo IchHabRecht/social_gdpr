69ecebde15653c5c53f835e217c7ef5d699ef21c
Nicole Cordes <typo3@cordes.co>
2020-04-24 12:48:23 +0200

    [TASK] Add basic extension files

---
+++ README.md
@@ -0,0 +1,21 @@
+# TYPO3 Extension social_gdpr
+
+[![Latest Stable Version](https://img.shields.io/packagist/v/ichhabrecht/social-gdpr.svg)](https://packagist.org/packages/ichhabrecht/social-gdpr)
+[![Build Status](https://img.shields.io/travis/IchHabRecht/social_gdpr/master.svg)](https://travis-ci.org/IchHabRecht/social_gdpr)
+[![StyleCI](https://styleci.io/repos/259281576/shield?branch=master)](https://styleci.io/repos/259281576)
+
+Show social media after user confirmation
+
+## Features
+
+## Installation
+
+Simply install the extension with Composer or the [Extension Manager](https://extensions.typo3.org/extension/social_gdpr/).
+
+`composer require ichhabrecht/social-gdpr`
+
+## Usage
+
+## Community
+
+- Thanks to [elementare teilchen GmbH](https://www.elementare-teilchen.de) that sponsors the maintenance of this extension
---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "social_gdpr".
+ *
+ * Auto generated 24-04-2020 12:47
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Social GDPR',
+  'description' => 'Show social media after user confirmation',
+  'category' => 'fe',
+  'author' => 'Nicole Cordes',
+  'author_email' => 'typo3@cordes.co',
+  'author_company' => 'biz-design',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearcacheonload' => 0,
+  'version' => '0.1.0',
+  'constraints' =>
+  array (
+    'depends' =>
+    array (
+      'typo3' => '8.7.0-8.7.99',
+    ),
+    'conflicts' =>
+    array (
+    ),
+    'suggests' =>
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:7:{s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"f2f3";s:13:"composer.json";s:4:"b987";s:12:"ext_icon.png";s:4:"e7f1";s:16:"phpunit.xml.dist";s:4:"041c";s:24:"sonar-project.properties";s:4:"ceba";s:36:"Resources/Public/Icons/Extension.svg";s:4:"9f9a";}',
+);
+
