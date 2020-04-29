0bed6f70c512bff828a4ea8465de894d0836e4f1
Nicole Cordes <typo3@cordes.co>
2020-04-29 12:59:30 +0200

    [RELEASE] Release of social_gdpr 0.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "social_gdpr".
  *
- * Auto generated 24-04-2020 12:47
+ * Auto generated 29-04-2020 12:59
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -22,19 +22,19 @@ $EM_CONF[$_EXTKEY] = array (
   'createDirs' => '',
   'clearcacheonload' => 0,
   'version' => '0.1.0',
-  'constraints' =>
+  'constraints' => 
   array (
-    'depends' =>
+    'depends' => 
     array (
       'typo3' => '8.7.0-8.7.99',
     ),
-    'conflicts' =>
+    'conflicts' => 
     array (
     ),
-    'suggests' =>
+    'suggests' => 
     array (
     ),
   ),
-  '_md5_values_when_last_written' => 'a:7:{s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"f2f3";s:13:"composer.json";s:4:"b987";s:12:"ext_icon.png";s:4:"e7f1";s:16:"phpunit.xml.dist";s:4:"041c";s:24:"sonar-project.properties";s:4:"ceba";s:36:"Resources/Public/Icons/Extension.svg";s:4:"9f9a";}',
+  '_md5_values_when_last_written' => 'a:29:{s:9:"ChangeLog";s:4:"135e";s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"f2f3";s:13:"composer.json";s:4:"3045";s:12:"ext_icon.png";s:4:"e7f1";s:17:"ext_localconf.php";s:4:"12d5";s:16:"phpunit.xml.dist";s:4:"041c";s:24:"sonar-project.properties";s:4:"ceba";s:36:"Classes/Handler/HandlerInterface.php";s:4:"49a6";s:25:"Classes/Handler/Match.php";s:4:"6f99";s:34:"Classes/Handler/YoutubeHandler.php";s:4:"ce89";s:40:"Classes/Hooks/ContentPostProcessHook.php";s:4:"8801";s:42:"Configuration/TCA/Overrides/tt_content.php";s:4:"ad18";s:38:"Configuration/TypoScript/constants.txt";s:4:"b455";s:34:"Configuration/TypoScript/setup.txt";s:4:"8bf4";s:27:"Resources/Private/.htaccess";s:4:"4adb";s:43:"Resources/Private/Language/de.locallang.xlf";s:4:"9bf7";s:46:"Resources/Private/Language/de.locallang_be.xlf";s:4:"83f8";s:40:"Resources/Private/Language/locallang.xlf";s:4:"bf50";s:43:"Resources/Private/Language/locallang_be.xlf";s:4:"c3d2";s:40:"Resources/Private/Templates/Youtube.html";s:4:"38c4";s:31:"Resources/Public/Css/styles.css";s:4:"4400";s:36:"Resources/Public/Icons/Extension.svg";s:4:"9f9a";s:47:"Resources/Public/Images/youtube_play_button.svg";s:4:"4acb";s:37:"Resources/Public/JavaScript/decode.js";s:4:"76eb";s:49:"Resources/Public/PHP/embetty-server-php/.htaccess";s:4:"523e";s:47:"Resources/Public/PHP/embetty-server-php/LICENSE";s:4:"7042";s:49:"Resources/Public/PHP/embetty-server-php/README.md";s:4:"b6b5";s:49:"Resources/Public/PHP/embetty-server-php/index.php";s:4:"7bf9";}',
 );
 
