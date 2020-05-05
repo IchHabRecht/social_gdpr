1057cdc7e221845eb4036937a69fabef064600e3
Nicole Cordes <typo3@cordes.co>
2020-05-05 17:14:16 +0200

    [BUGFIX] Downgrade Javascript to ES 5.1

--- Resources/Public/JavaScript/decode.js
+++ Resources/Public/JavaScript/decode.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const videos = document.querySelectorAll('.social-gdpr-youtube-video, .social-gdpr-vimeo-video');
-    videos.forEach(video => video.addEventListener('click', event => {
-        if (event.target.tagName.toLowerCase() !== 'a') {
-            const video = event.currentTarget;
-            video.parentNode.innerHTML = atob(video.children[0].getAttribute('data-iframe'));
-            event.preventDefault();
-        }
-    }))
+    var videos = document.querySelectorAll('.social-gdpr-youtube-video, .social-gdpr-vimeo-video');
+    videos.forEach(function(video) {
+        video.addEventListener('click', function(event) {
+            if (event.target.tagName.toLowerCase() !== 'a') {
+                var video = event.currentTarget;
+                video.parentNode.innerHTML = atob(video.children[0].getAttribute('data-iframe'));
+                event.preventDefault();
+            }
+        });
+    });
 });
