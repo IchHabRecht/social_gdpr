96be5eacae3fbad482789382736be174064411e0
Nicole Cordes <typo3@cordes.co>
2020-05-29 13:22:21 +0200

    [RELEASE] Release of social_gdpr 1.0.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "social_gdpr".
  *
- * Auto generated 26-05-2020 12:16
+ * Auto generated 29-05-2020 13:21
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearcacheonload' => 0,
-  'version' => '1.0.3',
+  'version' => '1.0.4',
   'constraints' => 
   array (
     'depends' => 
@@ -35,6 +35,6 @@ $EM_CONF[$_EXTKEY] = array (
     array (
     ),
   ),
-  '_md5_values_when_last_written' => 'a:34:{s:9:"ChangeLog";s:4:"2870";s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"f2f3";s:13:"composer.json";s:4:"0063";s:13:"composer.lock";s:4:"97bd";s:12:"ext_icon.png";s:4:"e7f1";s:17:"ext_localconf.php";s:4:"3137";s:16:"phpunit.xml.dist";s:4:"041c";s:24:"sonar-project.properties";s:4:"ceba";s:36:"Classes/Handler/HandlerInterface.php";s:4:"49a6";s:25:"Classes/Handler/Match.php";s:4:"6f99";s:32:"Classes/Handler/VimeoHandler.php";s:4:"11aa";s:34:"Classes/Handler/YoutubeHandler.php";s:4:"60fd";s:40:"Classes/Hooks/ContentPostProcessHook.php";s:4:"8801";s:37:"Classes/Service/VimeoImageService.php";s:4:"c0b4";s:39:"Classes/Service/YoutubeImageService.php";s:4:"d011";s:42:"Configuration/TCA/Overrides/tt_content.php";s:4:"ad18";s:38:"Configuration/TypoScript/constants.txt";s:4:"b455";s:34:"Configuration/TypoScript/setup.txt";s:4:"8bf4";s:27:"Resources/Private/.htaccess";s:4:"4adb";s:43:"Resources/Private/Language/de.locallang.xlf";s:4:"013f";s:46:"Resources/Private/Language/de.locallang_be.xlf";s:4:"83f8";s:40:"Resources/Private/Language/locallang.xlf";s:4:"dedf";s:43:"Resources/Private/Language/locallang_be.xlf";s:4:"c3d2";s:38:"Resources/Private/Templates/Vimeo.html";s:4:"d0e5";s:40:"Resources/Private/Templates/Youtube.html";s:4:"c847";s:31:"Resources/Public/Css/styles.css";s:4:"c0af";s:36:"Resources/Public/Icons/Extension.svg";s:4:"9f9a";s:39:"Resources/Public/Images/play_button.svg";s:4:"b4f5";s:47:"Resources/Public/Images/youtube_play_button.svg";s:4:"2aed";s:37:"Resources/Public/JavaScript/decode.js";s:4:"ba1a";s:40:"Tests/Unit/Handler/AbstractVideoTest.php";s:4:"ce8e";s:39:"Tests/Unit/Handler/VimeoHandlerTest.php";s:4:"39da";s:41:"Tests/Unit/Handler/YoutubeHandlerTest.php";s:4:"7fdc";}',
+  '_md5_values_when_last_written' => 'a:37:{s:9:"ChangeLog";s:4:"94b3";s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"f2f3";s:13:"composer.json";s:4:"58e6";s:13:"composer.lock";s:4:"97bd";s:12:"ext_icon.png";s:4:"e7f1";s:17:"ext_localconf.php";s:4:"3137";s:16:"phpunit.xml.dist";s:4:"041c";s:24:"sonar-project.properties";s:4:"ceba";s:36:"Classes/Handler/HandlerInterface.php";s:4:"49a6";s:25:"Classes/Handler/Match.php";s:4:"6f99";s:32:"Classes/Handler/VimeoHandler.php";s:4:"11aa";s:34:"Classes/Handler/YoutubeHandler.php";s:4:"60fd";s:40:"Classes/Hooks/ContentPostProcessHook.php";s:4:"8801";s:37:"Classes/Service/VimeoImageService.php";s:4:"c0b4";s:39:"Classes/Service/YoutubeImageService.php";s:4:"d011";s:42:"Configuration/TCA/Overrides/tt_content.php";s:4:"ad18";s:38:"Configuration/TypoScript/constants.txt";s:4:"b455";s:34:"Configuration/TypoScript/setup.txt";s:4:"8bf4";s:27:"Resources/Private/.htaccess";s:4:"4adb";s:43:"Resources/Private/Language/de.locallang.xlf";s:4:"013f";s:46:"Resources/Private/Language/de.locallang_be.xlf";s:4:"83f8";s:40:"Resources/Private/Language/locallang.xlf";s:4:"dedf";s:43:"Resources/Private/Language/locallang_be.xlf";s:4:"c3d2";s:38:"Resources/Private/Templates/Vimeo.html";s:4:"241c";s:40:"Resources/Private/Templates/Youtube.html";s:4:"ca02";s:31:"Resources/Public/Css/styles.css";s:4:"c0af";s:36:"Resources/Public/Icons/Extension.svg";s:4:"9f9a";s:39:"Resources/Public/Images/play_button.svg";s:4:"b4f5";s:47:"Resources/Public/Images/youtube_play_button.svg";s:4:"2aed";s:37:"Resources/Public/JavaScript/decode.js";s:4:"ba1a";s:27:"Tests/Fixtures/Content.html";s:4:"a2fd";s:52:"Tests/Functional/Fixtures/TypoScript/social_gdpr.txt";s:4:"6f43";s:53:"Tests/Functional/Hooks/ContentPostProcessHookTest.php";s:4:"ed09";s:40:"Tests/Unit/Handler/AbstractVideoTest.php";s:4:"e748";s:39:"Tests/Unit/Handler/VimeoHandlerTest.php";s:4:"9d18";s:41:"Tests/Unit/Handler/YoutubeHandlerTest.php";s:4:"d667";}',
 );
 
