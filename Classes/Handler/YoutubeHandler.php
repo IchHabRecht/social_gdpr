6e8ea771da4af66fde346f5677efea4e2304ee4e
Nicole Cordes <typo3@cordes.co>
2020-08-10 16:02:01 +0200

    !!![BUGFIX] Handle iframe height and width correctly

--- Classes/Handler/YoutubeHandler.php
+++ Classes/Handler/YoutubeHandler.php
@@ -6,6 +6,7 @@ namespace IchHabRecht\SocialGdpr\Handler;
 
 use IchHabRecht\SocialGdpr\Service\YoutubeImageService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\MathUtility;
 use TYPO3\CMS\Core\Utility\StringUtility;
 
 class YoutubeHandler implements HandlerInterface
@@ -47,8 +48,8 @@ class YoutubeHandler implements HandlerInterface
                         'uid' => StringUtility::getUniqueId(),
                         'id' => $match['id'],
                         'iframeHash' => base64_encode($match[0]),
-                        'height' => (int)($match['height'] ?? 0),
-                        'width' => (int)($match['width'] ?? 0),
+                        'height' => !empty($match['height']) ? (MathUtility::canBeInterpretedAsInteger($match['height']) ? $match['height'] . 'px' : $match['height']) : 0,
+                        'width' => !empty($match['width']) ? (MathUtility::canBeInterpretedAsInteger($match['width']) ? $match['width'] . 'px' : $match['width']) : 0,
                         'preview' => $this->youtubeImageService->getPreviewImage($match['id']),
                     ]
                 );
