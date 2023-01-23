be388a270db99b594756ab016f4504f57bb6f97a
Nicole Cordes <typo3@cordes.co>
2023-01-23 10:13:20 +0100

    [TASK] Raise compatibility to TYPO3 12.1

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3_MODE') || die();
+defined('TYPO3') || die('Access denied.');
 
 call_user_func(function () {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['social_gdpr'] =
