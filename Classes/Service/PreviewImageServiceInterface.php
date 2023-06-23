7157cf8c8069e92bda48d5566c490e388a314a81
Nikita Hovratov <nikita.h@live.de>
2023-06-23 13:28:36 +0200

    Fix cgl

--- Classes/Service/PreviewImageServiceInterface.php
+++ Classes/Service/PreviewImageServiceInterface.php
@@ -6,9 +6,9 @@ namespace IchHabRecht\SocialGdpr\Service;
 
 interface PreviewImageServiceInterface
 {
-    public function getPreviewImage(string $id) : string;
+    public function getPreviewImage(string $id): string;
 
-    public function deletePreviewImage(string $id) : void;
+    public function deletePreviewImage(string $id): void;
 
-    public function hasPreviewImage(string $id) : bool;
+    public function hasPreviewImage(string $id): bool;
 }
